19a82a5479f628db743d2dc10157f9e3aee88992
sephdm <github@seph.ca>
2026-04-26 20:53:29 +0000

    Enhance start-bridge.bat with automatic Administrator elevation

---
+++ start-bridge.bat
@@ -0,0 +1,29 @@
+@echo off
+:: Check for Administrator Privileges
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%~dp0"
+
+title YouTube Remote Bridge (ADMIN)
+echo ============================================================
+echo      YOUTUBE REMOTE - WINDOWS SYSTEM BRIDGE
+echo ============================================================
+echo.
+echo Starting Bridge service...
+node bridge/index.js
+echo.
+echo [!] Bridge process ended.
+pause
